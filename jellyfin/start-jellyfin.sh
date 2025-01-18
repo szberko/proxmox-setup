5a11d938cf4a48299d950e8a8c5a8ece24ce3c6d
Szabolcs Berkó <szberko@gmail.com>
2025-01-18 19:12:35 +0100

    remove docker run commands

--- jellyfin/start-jellyfin.sh
+++
@@ -1,8 +0,0 @@
-docker run -d \
-  --name=jellyfin \
-  -v ~/jellyfin/config:/config \
-  -v ~/jellyfin/cache:/cache \
-  -v ~/torrent:/media \
-  -p 8096:8096 \
-  --restart unless-stopped \
-  jellyfin/jellyfin:latest
